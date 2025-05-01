1a4a2730e09d8168b486b6fca3419bad75daf76b
Duy Nguyen <duynguyenquykhanh@gmail.com>
2025-04-30 20:29:21 -0700

    add get page

--- app.py
+++ app.py
@@ -144,6 +144,9 @@ def get_initial_map_data():
     # Return the data as JSON to the frontend
     return jsonify(hex_counts.to_dict(orient='records'))
 
+@app.route("/")
+def hello_world():
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=4000)  # Ensure it's listening on 0.0.0.0
